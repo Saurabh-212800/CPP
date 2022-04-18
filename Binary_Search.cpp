b7a6c53ad024a9ec09eb08f9953e371f0d72eb93
Saurabh Chavan <73703031+Saurabh-212800@users.noreply.github.com>
2022-04-18 19:46:27 +0530

    Create Binary_Search.cpp

---
+++ Binary_Search.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int binarysearch(int arr[], int n, int k){
+
+     int low = 0, high = n-1, mid;
+        while(low<=high){
+            mid = low + (high-low)/2;
+            if(arr[mid] == k){
+                return mid;
+            }
+            if(arr[mid]>k){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+    
+};
+
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i=0;i<N;i++)
+            cin>>arr[i];
+        int key;
+        cin>>key;
+        Solution ob;
+        int found = ob.binarysearch(arr,N,key);
+        cout<<found<<endl;
+    }
+}
