f5d677e9bc79a409eb3a0a4049642c093099c0a9
Saurabh Chavan <73703031+Saurabh-212800@users.noreply.github.com>
2023-02-26 17:58:54 +0530

    Create Reverse_String.java

---
+++ Java codes/Linear Search.java/Reverse_String.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+public class reverse { 
+    public static void main(String[] args)
+    {
+        StringBuffer obj = new StringBuffer("Saurabh");
+        System.out.println("Original String is = " +obj);
+
+        obj.reverse();
+        System.out.println("Reverse String is = " +obj);
+    }
+    
+}
